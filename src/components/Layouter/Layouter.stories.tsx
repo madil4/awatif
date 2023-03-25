634de2b35261f4c693b928d0c7ad2bb1aa2df574
Mohamed Adil <me@mohamedadil.com>
2023-03-25 21:33:54 +0100

    Chore: #98 should use storybook 7.0 (#99)

---
+++ src/components/Layouter/Layouter.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/html";
+import type { ComponentProps } from "solid-js";
+import { Layouter } from "./Layouter";
+
+type Story = StoryObj<any>;
+
+export const Default: Story = {
+  args: {
+    children: ["Editor", "ModelConfigurator", "Viewer"],
+  },
+};
+
+export default {
+  title: "Layouter",
+  render: (props) => <Layouter {...props} />,
+} as Meta<ComponentProps<typeof Layouter>>;
