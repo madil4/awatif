70abc87a9bd52bb4e84899df065c008d2db2e31e
Mohamed Adil <me@mohamedadil.com>
2023-05-06 15:31:24 +0200

    add orbit control (#121)

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { children, onMount } from "solid-js";
 
 export function Viewer(props: any) {
@@ -23,9 +24,14 @@ export function Viewer(props: any) {
 
       objects.forEach((object: any) => scene.add(object));
 
+      const controls = new OrbitControls(camera, renderer.domElement);
       camera.position.set(0, 1, 5);
+      controls.update();
 
       renderer.render(scene, camera);
+      controls.addEventListener("change", () => {
+        renderer.render(scene, camera);
+      });
     }, 0);
   });
 
