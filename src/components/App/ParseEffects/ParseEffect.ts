8abd1638b18340fcbdd10a3392589d5fcef0b7a4
Mohamed Adil <me@mohamedadil.com>
2023-05-12 11:38:46 +0200

    parse elements

--- src/components/App/ParseEffects/ParseEffect.ts
+++ src/components/App/ParseEffects/ParseEffect.ts
@@ -1,14 +1,18 @@
 import { createEffect } from "solid-js";
-import { setNodes, text } from "../store";
+import { setElements, setNodes, text } from "../store";
 
 export function ParseEffect() {
   createEffect(() => {
     import(createURL(text()))
       .then((module) => {
         setNodes(module.nodes ? module.nodes.filter(nodeFilter) : []);
+        setElements(
+          module.elements ? module.elements.filter(elementsFilter) : []
+        );
       })
       .catch((error) => {
         setNodes([]);
+        setElements([]);
         console.warn("Error importing module:", error);
       });
   });
@@ -18,3 +22,4 @@ const createURL = (text: string): string =>
   URL.createObjectURL(new Blob([text], { type: "application/javascript" }));
 
 const nodeFilter = (node: number[]) => node.length == 3;
+const elementsFilter = (element: number[]) => element.length == 2;
