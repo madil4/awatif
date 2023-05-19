7e904299affb8748a98952a76a2ae27e97ca44e7
Mohamed Adil <me@mohamedadil.com>
2023-05-19 10:22:45 +0200

    dispose supports

--- src/components/App/store.ts
+++
@@ -1,6 +0,0 @@
-import { createSignal } from "solid-js";
-
-export const [nodes, setNodes] = createSignal([]);
-export const [elements, setElements] = createSignal([]);
-export const [text, setText] = createSignal("");
-export const [supports, setSupports] = createSignal([]);
