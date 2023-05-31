ed8d1fa81dc08b5c4a15f8b6d591c9b6bafe5407
Mohamed Adil <me@mohamedadil.com>
2023-05-31 21:16:54 +0200

    enable chromatic turbosnap

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,18 @@
+import { mergeConfig } from "vite";
+import turbosnap from "vite-plugin-turbosnap";
+
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   framework: {
     name: "@storybook/html-vite",
+  },
+  core: { builder: "@storybook/builder-vite" },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins:
+        configType === "PRODUCTION"
+          ? [turbosnap({ rootDir: config.root ?? process.cwd() })]
+          : [],
+    });
   },
 };
