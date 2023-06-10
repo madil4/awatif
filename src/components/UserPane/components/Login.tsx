0ed31c6d25cbd26f1e931b12e80321654c8309ba
Mohamed Adil <me@mohamedadil.com>
2023-06-10 16:08:53 +0200

    use a instead of button

--- src/components/UserPane/components/Login.tsx
+++ src/components/UserPane/components/Login.tsx
@@ -7,10 +7,7 @@ export function Login(props: LoginProps) {
   return (
     <>
       <p>Sign in to your account</p>
-      <button
-        class="btn btn-block btn-neutral mt-2"
-        onclick={props.onGoogleClick}
-      >
+      <a class="btn btn-sm btn-neutral mt-2" onclick={props.onGoogleClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -20,8 +17,8 @@ export function Login(props: LoginProps) {
           <path d="M128,228A100,100,0,1,1,198.71069,57.28906,12.0001,12.0001,0,1,1,181.74,74.25977,75.99547,75.99547,0,1,0,203.05371,140H128a12,12,0,0,1,0-24h88a12,12,0,0,1,12,12A100.11332,100.11332,0,0,1,128,228Z" />
         </svg>
         Continue with Google
-      </button>
-      <button class="btn btn-block btn-neutral" onclick={props.onAzureClick}>
+      </a>
+      <a class="btn btn-sm btn-neutral mt-2" onclick={props.onAzureClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -34,7 +31,7 @@ export function Login(props: LoginProps) {
           <path d="M266.89,265.61H479.7v212.8H266.89Z" />
         </svg>
         Continue with Microsoft
-      </button>
+      </a>
     </>
   );
 }
