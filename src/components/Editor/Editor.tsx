1abab34d27329cfb5b53ee58e2d26d60e3a94b4d
Mohamed Adil <me@mohamedadil.com>
2023-06-10 23:53:57 +0200

    fix loading algorithm

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { Component, onMount } from "solid-js";
+import { Component, createEffect, onMount } from "solid-js";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import "./useWorker";
 
@@ -10,18 +10,20 @@ type EditorProps = {
 export const Editor: Component<EditorProps> = (props) => {
   let container: HTMLDivElement;
 
-  onMount(() => {
-    const editor = monaco.editor.create(container, {
-      value: props.text,
-      automaticLayout: true,
-      minimap: { enabled: false },
-      theme: "vs-dark",
-      language: "typescript",
-    });
+  createEffect(() => {
+    if (props.text) {
+      const editor = monaco.editor.create(container, {
+        value: props.text,
+        automaticLayout: true,
+        minimap: { enabled: false },
+        theme: "vs-dark",
+        language: "typescript",
+      });
 
-    editor.onDidChangeModelContent(() => {
-      if (props.onTextChange) props.onTextChange(editor.getValue());
-    });
+      editor.onDidChangeModelContent(() => {
+        if (props.onTextChange) props.onTextChange(editor.getValue());
+      });
+    }
   });
 
   return (
