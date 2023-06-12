af41c78e34eac0043deabad29013622a727b9b1e
Mohamed Adil <me@mohamedadil.com>
2023-06-12 22:42:42 +0200

    update awatif package (#177)

---
+++ algorithms/README.md
@@ -0,0 +1,1 @@
+I'm building an online parametric tool for structural engineers. If you'd like to help, please reach out to me on LinkedIn at https://www.linkedin.com/in/madil4/
--- algorithms/vite.config.ts
+++ algorithms/vite.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "../public/",
     lib: {
       entry: "src/index.ts",
-      name: "algorithms",
       formats: ["es"],
-      fileName: (format) => `algorithms-${format}-[hash].js`,
     },
   },
 });
