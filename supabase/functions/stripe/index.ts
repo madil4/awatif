74ac0d83afc6c3791822765bbef51f04d15050eb
Mohamed Adil <me@mohamedadil.com>
2023-06-29 13:14:14 +0200

    add stripe webhook

--- supabase/functions/stripe/index.ts
+++ supabase/functions/stripe/index.ts
@@ -1,14 +1,25 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import Stripe from "https://esm.sh/stripe?target=deno";
 
-console.log("Hello from Functions!");
+const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"));
+const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
-serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+console.log(`Stripe function initialized`);
 
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  });
+serve(async (request) => {
+  const body = await request.text();
+  let receivedEvent;
+  try {
+    receivedEvent = await stripe.webhooks.constructEventAsync(
+      body,
+      request.headers.get("Stripe-Signature"),
+      Deno.env.get("STRIPE_WEBHOOK_SIGNING_SECRET"),
+      undefined,
+      cryptoProvider
+    );
+  } catch (err) {
+    return new Response(err.message, { status: 400 });
+  }
+
+  return new Response(JSON.stringify(receivedEvent), { status: 200 });
 });
