92aa8c24ad9b02eeddd143fae4ac420faab3383d
Mohamed Adil <me@mohamedadil.com>
2023-06-29 15:12:09 +0200

    add email to checkout (#202)

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -2,13 +2,15 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from "https://esm.sh/stripe?target=deno";
 
 const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"));
-const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
 console.log(`Stripe-checkout function initialized`);
 
 serve(async (request) => {
+  const body = await request.formData();
+
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
+    customer_email: body.get("email"),
     line_items: [
       {
         price: "price_1NO5QpJwIWdjwnrnn4NGJ6z0",
