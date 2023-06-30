b2b3498da4d5c115188b6df7402568e40e70fdb8
Mohamed Adil <me@mohamedadil.com>
2023-06-30 02:03:43 +0200

    fullfil user

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,18 +1,19 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from "https://esm.sh/stripe?target=deno";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"));
 const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
 console.log(`Stripe function initialized`);
 
-serve(async (request) => {
-  const body = await request.text();
-  let receivedEvent;
+serve(async (req) => {
+  const body = await req.text();
+  let event;
   try {
-    receivedEvent = await stripe.webhooks.constructEventAsync(
+    event = await stripe.webhooks.constructEventAsync(
       body,
-      request.headers.get("Stripe-Signature"),
+      req.headers.get("Stripe-Signature"),
       Deno.env.get("STRIPE_WEBHOOK_SIGNING_SECRET"),
       undefined,
       cryptoProvider
@@ -21,5 +22,35 @@ serve(async (request) => {
     return new Response(err.message, { status: 400 });
   }
 
-  return new Response(JSON.stringify(receivedEvent), { status: 200 });
+  const supabase = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
+  );
+
+  const customer = await stripe.customers.retrieve(event.data.object.customer);
+  switch (event.type) {
+    case "customer.subscription.created":
+      console.log("subscription created");
+      await supabase
+        .from("users")
+        .update({ phone: customer.id })
+        .eq("email", customer.email);
+      break;
+    case "customer.subscription.updated":
+      console.log("subscription updated");
+      await supabase
+        .from("users")
+        .update({ phone: customer.id })
+        .eq("email", customer.email);
+      break;
+    case "customer.subscription.deleted":
+      console.log("subscription deleted");
+      await supabase
+        .from("users")
+        .update({ phone: null })
+        .eq("email", customer.email);
+      break;
+  }
+
+  return new Response(JSON.stringify({ received: true }), { status: 200 });
 });
