3f3dd08eb5663c7a2add6d33954dade3f13a941a
Mohamed Adil <me@mohamedadil.com>
2023-07-30 00:48:41 +0200

    add basic beam visualization

--- src/Viewer/objects/ElementResults2.stories.tsx
+++ src/Viewer/objects/ElementResults2.stories.tsx
@@ -13,30 +13,87 @@ export default {
       <Viewer>
         <Grid />
 
-        <ElementResult2 start={undefined} end={undefined} result={[10, 10]} />
-        <ElementResult2 start={[0, 0]} end={[5, 0]} result={[10, 10]} />
+        <ElementResult2
+          start={undefined}
+          end={undefined}
+          result={[10, 10]}
+          degree={0}
+        />
+        <ElementResult2
+          start={[0, 0]}
+          end={[5, 0]}
+          result={[10, 10]}
+          degree={0}
+        />
         <ElementResult2
           start={[0, 0, "k"]}
           end={[5, 0, "k"]}
           result={[10, 10]}
+          degree={0}
         />
-        <ElementResult2 start={[0, , 0]} end={[5, , 0]} result={[10, 10]} />
-        <ElementResult2 start={[0, 0, 0]} end={[-3, 0, 1]} result={["", 10]} />
+        <ElementResult2
+          start={[0, , 0]}
+          end={[5, , 0]}
+          result={[10, 10]}
+          degree={0}
+        />
+        <ElementResult2
+          start={[0, 0, 0]}
+          end={[-3, 0, 1]}
+          result={["", 10]}
+          degree={0}
+        />
+        <ElementResult2
+          start={[0, 0, 0]}
+          end={[-3, 0, 1]}
+          result={[10, 10]}
+          degree={""}
+        />
 
         {/* constant  */}
-        <ElementResult2 start={[0, 0, 0]} end={[5, 0, 0]} result={[1, 1]} />
+        <ElementResult2
+          start={[0, 0, 0]}
+          end={[5, 0, 0]}
+          result={[1, -1]}
+          degree={0}
+        />
         <Element start={[0, 0, 0]} end={[5, 0, 0]} />
-        <ElementResult2 start={[0, 0, 3]} end={[5, 0, 3]} result={[-1, -1]} />
+        <ElementResult2
+          start={[0, 0, 3]}
+          end={[5, 0, 3]}
+          result={[-1, 1]}
+          degree={0}
+        />
         <Element start={[0, 0, 3]} end={[5, 0, 3]} />
 
         {/* linear  */}
-        <ElementResult2 start={[-6, 0, 6]} end={[-1, 0, 6]} result={[0, -1]} />
+        <ElementResult2
+          start={[-6, 0, 6]}
+          end={[-1, 0, 6]}
+          result={[0, -1]}
+          degree={1}
+        />
         <Element start={[-6, 0, 6]} end={[-1, 0, 6]} />
-        <ElementResult2 start={[-6, 0, 4]} end={[-1, 0, 4]} result={[1, 0.5]} />
+        <ElementResult2
+          start={[-6, 0, 4]}
+          end={[-1, 0, 4]}
+          result={[1, 0.5]}
+          degree={1}
+        />
         <Element start={[-6, 0, 4]} end={[-1, 0, 4]} />
-        <ElementResult2 start={[-6, 0, 2]} end={[-1, 0, 2]} result={[1, -1]} />
+        <ElementResult2
+          start={[-6, 0, 2]}
+          end={[-1, 0, 2]}
+          result={[1, -1]}
+          degree={1}
+        />
         <Element start={[-6, 0, 2]} end={[-1, 0, 2]} />
-        <ElementResult2 start={[-6, 0, 0]} end={[-1, 0, 0]} result={[-1, 1]} />
+        <ElementResult2
+          start={[-6, 0, 0]}
+          end={[-1, 0, 0]}
+          result={[-1, 1]}
+          degree={1}
+        />
         <Element start={[-6, 0, 0]} end={[-1, 0, 0]} />
       </Viewer>
     </div>
