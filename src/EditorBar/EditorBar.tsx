32fcf1ecdcb1d62c52a72979552a4cfb6430592d
Mohamed Adil <me@mohamedadil.com>
2023-07-29 17:22:18 +0200

    add help panel

--- src/EditorBar/EditorBar.tsx
+++ src/EditorBar/EditorBar.tsx
@@ -24,14 +24,6 @@ export function EditorBar(props: EditorBarProps) {
             </button>
           </div>
         </Show>
-        <div class="badge badge-sm badge-neutral mt-2 ml-2">
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSc9k3Wv6jbPreGL39C7-0piytRyO9AoQlDvXoonI-sBf_M0EQ/viewform"
-            target="_blank"
-          >
-            In Beta, Feedback?
-          </a>
-        </div>
       </div>
       <div class="flex flex-row space-x-2 mr-2 mt-[0.15rem]">
         {/* @ts-ignore */}
@@ -52,6 +44,82 @@ export function EditorBar(props: EditorBarProps) {
           <Upgrade />
         </Show>
         <MyProjects />
+        <Help />
+      </div>
+    </div>
+  );
+}
+
+function Help() {
+  return (
+    <div class="dropdown dropdown-end">
+      <a tabindex="0" class="btn btn-xs btn-neutral">
+        <span>Help</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+          <line x1="12" y1="17" x2="12.01" y2="17"></line>
+        </svg>
+      </a>
+
+      <div
+        tabindex="0"
+        class="z-50 dropdown-content card card-compact w-80 p-2 bg-base-100 "
+      >
+        <div class="card-body">
+          <a
+            tabindex="0"
+            class="btn btn-sm btn-neutral"
+            href="https://mohamedadil.notion.site/Getting-started-a8565ccbdea641b7b4793a6f42d983af?pvs=4"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+              <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+            </svg>
+            <span class="capitalize">Documentation</span>
+          </a>
+          <a
+            tabindex="0"
+            class="btn btn-sm btn-neutral"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSc9k3Wv6jbPreGL39C7-0piytRyO9AoQlDvXoonI-sBf_M0EQ/viewform"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+            </svg>
+            <span class="capitalize">Feedback</span>
+          </a>
+        </div>
       </div>
     </div>
   );
