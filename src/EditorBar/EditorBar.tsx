01fe1937085ae3d7a18288e4b557fc2979212975
Mohamed Adil <me@mohamedadil.com>
2023-07-31 23:17:14 +0200

    add blog and examples (#248)

--- src/EditorBar/EditorBar.tsx
+++ src/EditorBar/EditorBar.tsx
@@ -82,7 +82,27 @@ function Help() {
       >
         <div class="card-body">
           <a
-            tabindex="0"
+            class="btn btn-sm btn-neutral"
+            href="https://awatif.co/examples"
+            target="_blank"
+          >
+            <svg
+              viewBox="0 0 16 16"
+              width="16"
+              height="16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="m13.429 8-4.681 2.702-4.681 2.703V2.594l4.68 2.703 4.682 2.702Z"
+                stroke="currentColor"
+                stroke-miterlimit="10"
+                stroke-linejoin="bevel"
+              ></path>
+            </svg>
+            <span class="capitalize">Examples</span>
+          </a>
+          <a
             class="btn btn-sm btn-neutral"
             href="https://mohamedadil.notion.site/Getting-started-a8565ccbdea641b7b4793a6f42d983af?pvs=4"
             target="_blank"
@@ -103,7 +123,27 @@ function Help() {
             <span class="capitalize">Documentation</span>
           </a>
           <a
-            tabindex="0"
+            class="btn btn-sm btn-neutral"
+            href="https://awatif.co/blog"
+            target="_blank"
+          >
+            <svg
+              viewBox="0 0 16 16"
+              width="16"
+              height="16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13 12V5l-.009-3H3v10.5m0 0A1.5 1.5 0 0 0 4.5 14H13v-1.65L12.991 11H4.5A1.5 1.5 0 0 0 3 12.5ZM5.005 2v4l1.5-1.695L8.005 6V2h-3Z"
+                stroke="currentColor"
+                stroke-miterlimit="10"
+                stroke-linejoin="bevel"
+              ></path>
+            </svg>
+            <span class="capitalize">Blog</span>
+          </a>
+          <a
             class="btn btn-sm btn-neutral"
             href="https://docs.google.com/forms/d/e/1FAIpQLSc9k3Wv6jbPreGL39C7-0piytRyO9AoQlDvXoonI-sBf_M0EQ/viewform"
             target="_blank"
