c519d1c3c22c9f1aa03790f782817eb5b7ed0445
Mohamed Adil <me@mohamedadil.com>
2024-02-18 19:21:43 +0100

    add examples

--- docs/eleventy.config.js
+++ docs/eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/**/*.css");
   eleventyConfig.addPassthroughCopy("src/**/*.js");
+  eleventyConfig.addPassthroughCopy("src/**/*.jpg");
 
   eleventyConfig.setServerOptions({
     module: "@11ty/eleventy-server-browsersync",
