514195a41c7a706405189334f002111342baf15a
Mohamed Adil <me@mohamedadil.com>
2024-02-18 15:34:33 +0100

    docs: manual deploy to Netlify (#285)
    * serve with open

--- docs/eleventy.config.js
+++ docs/eleventy.config.js
@@ -2,6 +2,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/**/*.css");
   eleventyConfig.addPassthroughCopy("src/**/*.js");
 
+  eleventyConfig.setServerOptions({
+    module: "@11ty/eleventy-server-browsersync",
+
+    port: 4600,
+    open: true,
+  });
+
   return {
     dir: {
       input: "src",
