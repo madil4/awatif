2aa45c08f14ba52d3a0579a290e34871c238135c
Mohamed Adil <me@mohamedadil.com>
2024-05-17 19:54:59 +0200

    use unstrict mode for katext

--- awatif-design/src/ec/timber/utils/renderMath.ts
+++ awatif-design/src/ec/timber/utils/renderMath.ts
@@ -1,8 +1,6 @@
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 import katex from "katex";
 
-// reference: https://katex.org/docs/supported
-
 export function renderMath(latex: string) {
-  return unsafeHTML(katex.renderToString(latex));
+  return unsafeHTML(katex.renderToString(latex, { strict: false }));
 }
