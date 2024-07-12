2c2ec139bba8f9d3bb68308ac26977a2d1d90211
Mohamed Adil <me@mohamedadil.com>
2024-07-12 19:00:49 +0200

    update webiste due to use of maps

--- awatif-data-structure/src/index.ts
+++ awatif-data-structure/src/index.ts
@@ -1,6 +1,6 @@
-// The geometry of any structure can be represented by these two simple entities.
-export type Node = [number, number, number]; // Represents the position coordinates [x,y,z]
-export type Element = [number, number]; // Represents the indices of the first and second node in the list of nodes
+// The geometry of any structure can be represented by these two entities:
+export type Node = [number, number, number]; // position coordinates [x,y,z]
+export type Element = [number, number]; // indices of the first and second node in the list of nodes
 
 // Analysis Inputs
 export type AnalysisInputs = {
