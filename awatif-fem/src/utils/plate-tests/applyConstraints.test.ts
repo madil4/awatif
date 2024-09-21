e0a26183f949f90596082b35c993524533b0e108
gottn <musab.ahmed1996@gmail.com>
2024-09-21 19:08:34 +0200

    Add applyConstriants function and its tests

---
+++ awatif-fem/src/utils/plate-tests/applyConstraints.test.ts
@@ -0,0 +1,27 @@
+import { applyConstraints } from '../applyConstraints';
+
+test('applyConstraints modifies kk and ff correctly', () => {
+  // Initial matrices
+  const kk = [
+    [10, 2, 3],
+    [2, 20, 6],
+    [3, 6, 30],
+  ];
+  const ff = [1, 2, 3];
+  const bcdof = [1]; // Constrain DOF at index 1
+
+  // Expected results
+  const expectedKk = [
+    [10, 0, 3],
+    [0, 1, 0],
+    [3, 0, 30],
+  ];
+  const expectedFf = [1, 0, 3];
+
+  // Apply constraints
+  applyConstraints(kk, ff, bcdof);
+
+  // Assertions
+  expect(kk).toEqual(expectedKk);
+  expect(ff).toEqual(expectedFf);
+});
