3c74c2aa0b335beb32257ed00f639166e4a7dec7
Mohamed Adil <me@mohamedadil.com>
2024-11-01 16:52:34 +0100

    add 2d mesh example to website

--- website/eleventy.config.js
+++ website/eleventy.config.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/**/*.js");
   eleventyConfig.addPassthroughCopy("src/**/*.jpg");
   eleventyConfig.addPassthroughCopy("src/**/*.ico");
+  eleventyConfig.addPassthroughCopy("src/**/*.wasm");
 
   eleventyConfig.setServerOptions({
     module: "@11ty/eleventy-server-browsersync",
