7bf9fdda371f3fdc1afff79ad26b2ffa6e8bfcce
Mohamed Adil <me@mohamedadil.com>
2024-11-17 14:50:08 +0100

    add types for Building object

--- awatif-data-structure/src/index.ts
+++ awatif-data-structure/src/index.ts
@@ -1,5 +1,7 @@
 import { State } from "vanjs-core";
+import { TemplateResult } from "lit-html";
 
+// Analytical Model
 // The geometry of any structure can be represented by these two entities:
 export type Node = [number, number, number]; // position coordinates [x,y,z]
 export type Element = number[]; // indices of the first and second node in the list of nodes
@@ -48,7 +50,64 @@ type ElementAnalysisOutputs = {
   bendingZ?: [number, number];
 };
 
+// High-order Model
+type ColumnAnalysisInput = {
+  load: unknown;
+  support: unknown;
+  section: unknown;
+  material: unknown;
+};
+type ColumnAnalysisOutput = {
+  bending: unknown;
+  axial: unknown;
+};
+
+type EcTimberColumnDesignInput = {
+  buildingClass: string;
+  strength: number;
+};
+
+type EcTimberColumnDesignOutput = {
+  slendernessRatio: number;
+  utilizationFactor: number;
+};
+
+type ColumnDesignInput = EcTimberColumnDesignInput;
+type ColumnDesignOutput = EcTimberColumnDesignOutput;
+
+// Todo: think of way to separate the generic type ColumnAnalysisInput from the remaining specific onces
+// Todo: maybe better to separate functions from data
+type ColumnData = {
+  analysisInput?: ColumnAnalysisInput;
+  analysisOutput?: ColumnAnalysisOutput;
+  designInput?: ColumnDesignInput;
+  designOutput?: ColumnDesignOutput;
+  script?: (
+    analysisInput: ColumnAnalysisInput,
+    designInput: ColumnDesignInput
+  ) => ColumnDesignOutput;
+  report?: (
+    analysisInput: ColumnAnalysisInput,
+    designInput: ColumnDesignInput,
+    analysisOutput: ColumnAnalysisOutput,
+    designOutput: ColumnDesignOutput
+  ) => TemplateResult;
+  visualObject?: (inputs: unknown) => unknown;
+};
+
+export type Building = {
+  points: State<[number, number, number][]>; // all the points used to define stories, floors, ..etc
+  stories: State<number[]>; // example [1,2,3] three stories defined by three points indices from the points list
+  columns: State<Map<number, number[]>>; // example 2 -> [1,2,3] the keys of this map represent the story index
+  // and the value is a list of point indices represent the column location at this story
+  slabs: State<Map<number, number[][]>>; // example 2 -> [[1,2,3],[4,5,6,7]] the keys of this map represent the story index
+  // and the value is a list of polygons at this story represented by point indices
+  columnData: State<Map<[number, number], ColumnData>>;
+  slabData: State<Map<[number, number], ColumnData>>;
+};
+
 export type Structure = {
+  building?: Building;
   nodes?: State<Node[]>;
   elements?: State<Element[]>;
   analysisInputs?: State<AnalysisInputs>;
