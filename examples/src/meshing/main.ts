2d662c2d19b535cc3aa267a773867fbcba15daf2
kaison428 <cheungsiukaison.soniacheung@gmail.com>
2025-01-25 22:54:20 -0500

    Add column mesh to viewer

--- examples/src/meshing/main.ts
+++ examples/src/meshing/main.ts
@@ -5,26 +5,34 @@ import { meshing } from "./meshing";
 
 const building: Building = {
   points: van.state([
-    [0, 0, 1],
-    [10, 0, 1],
-    [10, 10, 1],
-    [0, 10, 1],
+    [0, 0, 0],
     [0, 0, 3],
     [10, 0, 3],
     [10, 10, 3],
     [0, 10, 3],
+    [0, 0, 6],
+    [10, 0, 6],
+    [10, 10, 6],
+    [0, 10, 6],
   ]),
-  stories: van.state([0, 4]),
-  columns: van.state([0, 1, 2, 3]),
+  stories: van.state([0, 1, 5]),
+  columns: van.state([1, 2, 3, 4]),
   slabs: van.state([
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
   ]),
-  columnsByStory: van.state(new Map([[0, [0, 1, 2, 3]]])),
+  columnsByStory: van.state(
+    new Map([
+      [0, []],
+      [1, [0, 1, 2, 3]],
+      [2, [0, 1, 2, 3]],
+    ])
+  ),
   slabsByStory: van.state(
     new Map([
-      [0, [0]],
-      [1, [1]],
+      [0, []],
+      [1, [0]],
+      [2, [1]],
     ])
   ),
   columnData: van.state(
