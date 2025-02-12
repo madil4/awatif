3d890cf7efd808b7b94d57c4c486577cb480bb70
Mohamed Adil <me@mohamedadil.com>
2025-02-12 21:29:14 +0100

    review changes

--- examples/src/report/main.ts
+++ examples/src/report/main.ts
@@ -1,15 +1,17 @@
-import { Node, Element, AnalyzeOutputs, DeformOutputs, ElementInputs, NodeInputs, Structure } from "awatif-data-structure";
+import {
+  Node,
+  Element,
+  AnalyzeOutputs,
+  DeformOutputs,
+  ElementInputs,
+  NodeInputs,
+} from "awatif-data-structure";
 import { deform, analyze } from "awatif-fem";
-import { html, TemplateResult } from "lit-html";
 import van from "vanjs-core";
 import { State } from "vanjs-core/debug";
 import { Parameters, parameters, viewer } from "awatif-ui";
+import { template } from "./template";
 
-//const nodes = van.state([0, 0, 1]);
-//@ts-ignorets-ignore
-import logo from "./awatif-logo.jpg";
-
-//bars
 // Init
 const params: Parameters = {
   xPosition: { value: van.state(600), min: 0, max: 1000 },
@@ -22,6 +24,15 @@ const nodeInputs: State<NodeInputs> = van.state({});
 const elementInputs: State<ElementInputs> = van.state({});
 const deformOutputs: State<DeformOutputs> = van.state({});
 const analyzeOutputs: State<AnalyzeOutputs> = van.state({});
+
+const structure = {
+  nodes,
+  elements,
+  nodeInputs,
+  elementInputs,
+  deformOutputs,
+  analyzeOutputs,
+};
 
 // Events: on parameter change
 van.derive(() => {
@@ -69,167 +80,16 @@ van.derive(() => {
   );
 });
 
-const nodeIDsArray = [...nodeInputs.val.supports.keys()];
-
-const noNodes = nodes.val.length
-const indexNodes = Array.from({ length: noNodes }, (_, i) => i);
-
-const noBars = elements.val.length
-const indexBars = Array.from({ length: noBars }, (_, i) => i);
-
-
-
-
-const template: (nodes: Structure["nodes"]) => TemplateResult = (nodes) => {
-  return html`
-  <br>
-  <header class="header">
-    <div class="header-left">
-      <h6>Report</h6>
-      <p class="bolt">
-        <a href="https://awatif.co" target="_blank" rel="noopener noreferrer">Awatif.co</a>
-      </p>
-      <p class="normal" id="reportDate"></p> <!-- Dynamic Date -->
-    </div>
-    <div class="header-right">
-      <img src=${logo} id="headerLogo" height="60px" />
-    </div>
-  </header>
-
-    <br>
-    <h1>Bars</h1>
-
-    <br>
-    <h2>Nodes</h2>
-    <p class="text">The following table gives an overview of the node coordinates.</p>
-    <br>
-
-    <!-- Table Section -->
-    <table id="data-table">
-      <tr>
-        <th>Node</th>
-        <th>xCoord</th>
-        <th>yCoord</th>
-        <th>zCoord</th>
-      </tr>
-      ${indexNodes.map(
-        (iNodes) => html`
-          <tr>
-          <td><div class="custom-cell-content">${iNodes}</div></td>
-          <td><div class="custom-cell-content">${nodes.val[iNodes][0]}</div></td>
-          <td><div class="custom-cell-content">${nodes.val[iNodes][1]}</div></td>
-          <td><div class="custom-cell-content">${nodes.val[iNodes][2]}</div></td>
-      </tr>
-        `
-      )}
-    </table>
-
-    <br>
-    <h2>Supports</h2>
-    <p class="text">The following table gives an overview of the support conditions.</p>
-    <br>
-
-    <!-- Table Section -->
-    <table id="data-table">
-      <tr>
-        <th>Node</th>
-        <th>ux</th>
-        <th>uy</th>
-        <th>uz</th>
-        <th>mx</th>
-        <th>my</th>
-        <th>mz</th>
-      </tr>
-      ${nodeIDsArray.map(
-        
-        (iBars) => html`
-      <tr>
-          <td><div class="custom-cell-content">${iBars}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[0]}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[1]}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[2]}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[3]}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[4]}</div></td>
-          <td><div class="custom-cell-content">${nodeInputs.val.supports.get(iBars)[5]}</div></td>
-     </tr>
-        `
-      )}
-    </table>
-
-    <br>
-    <h2>Reactions</h2>
-    <p class="text">The following table gives an overview of the reaction forces.</p>
-    <br>
-
-    <!-- Table Section -->
-    <table id="data-table">
-      <tr>
-        <th>Node</th>
-        <th>Fx</th>
-        <th>Fy</th>
-        <th>Fz</th>
-      </tr>
-      ${nodeIDsArray.map(
-        
-        (iBars) => html`
-      <tr>
-          <td><div class="custom-cell-content">${iBars}</div></td>
-          <td><div class="custom-cell-content">${deformOutputs.val.reactions.get(iBars)[0].toFixed(0)}</div></td>
-          <td><div class="custom-cell-content">${deformOutputs.val.reactions.get(iBars)[1].toFixed(0)}</div></td>
-          <td><div class="custom-cell-content">${deformOutputs.val.reactions.get(iBars)[2].toFixed(0)}</div></td>
-     </tr>
-        `
-      )}
-    </table>
-
-    <br>
-    <h2>Elements</h2>
-    <p class="text">The following table gives an overview of the element results.</p>
-    <br>
-
-    <!-- Table Section -->
-    <table id="data-table">
-      <tr>
-        <th>Bar</th>
-        <th>Area</th>
-        <th>Normal</th>
-      </tr>
-      ${indexBars.map(
-        (iBars) => html`
-          <tr>
-          <td><div class="custom-cell-content">${iBars}</div></td>
-          <td><div class="custom-cell-content">${elementInputs.val.areas.get(iBars)}</div></td>
-          <td><div class="custom-cell-content">${analyzeOutputs.val.normals.get(iBars)[0].toFixed(0)}</div></td>
-      </tr>
-        `
-      )}
-    </table>
-    <br><br><br>
-  `
-  ;
-};
-
 document.body.append(
   parameters(params),
   viewer({
-    structure: {
-      nodes,
-      elements,
-      nodeInputs,
-      elementInputs,
-      deformOutputs,
-      analyzeOutputs,
-    },
+    structure,
     settingsObj: {
       gridSize: 1000,
     },
     reportObj: {
       template,
-      data: nodes,
+      data: structure,
     },
   })
 );
-
-const options = { day: "numeric", month: "long", year: "numeric" };
-document.getElementById("reportDate").textContent = new Date().toLocaleDateString("en-US", options);
-
