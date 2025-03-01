83d191ae902c997ab3a9c02e8a7ac0fa63f35729
Mohamed Adil <me@mohamedadil.com>
2025-03-01 23:15:00 +0100

    add plate example

--- examples/src/plate/main.ts
+++ examples/src/plate/main.ts
@@ -4,42 +4,68 @@ import {
   Element,
   NodeInputs,
   ElementInputs,
+  DeformOutputs,
 } from "awatif-data-structure";
 import { viewer } from "awatif-ui";
 import { deform } from "awatif-fem";
+import { mesh } from "awatif-mesh";
 
-const nodes: State<Node[]> = van.state([
-  [0, 0, 0],
-  [0, 5, 0],
-  [5, 2.5, 0],
-]);
-const elements: State<Element[]> = van.state([[0, 1, 2]]);
+const nodesState: State<Node[]> = van.state([]);
+const elementsState: State<Element[]> = van.state([]);
+const nodeInputsState: State<NodeInputs> = van.state({});
+const elementInputsState: State<ElementInputs> = van.state({});
+const deformOutputsState: State<DeformOutputs> = van.state({});
 
-const nodeInputs: State<NodeInputs> = van.state({
-  supports: new Map([
-    [0, [true, true, true, true, true, true]],
-    [1, [true, true, true, true, true, true]],
-  ]),
-  loads: new Map([[2, [0, 0, -10, 0, 0, 0]]]),
+van.derive(() => {
+  const { nodes, elements } = mesh({
+    points: van.state([
+      [0, 0],
+      [10, 0],
+      [15, 5],
+      [0, 20],
+      [5, 2.5],
+    ]),
+    polygon: van.state([0, 1, 2, 3]),
+  });
+
+  const nodeInputs: NodeInputs = {
+    supports: new Map(
+      [0, 1, 2, 3].map((node) => [node, [true, true, true, true, true, true]])
+    ),
+    loads: new Map([[4, [0, 0, -300, 0, 0, 0]]]),
+  };
+  const elementInputs: ElementInputs = {
+    elasticities: new Map(elements.val.map((_, i) => [i, 100])),
+    thicknesses: new Map(elements.val.map((_, i) => [i, 1])),
+    poissonsRatios: new Map(elements.val.map((_, i) => [i, 0.3])),
+  };
+
+  const deformOutputs = deform(
+    nodes.val,
+    elements.val,
+    nodeInputs,
+    elementInputs
+  );
+
+  // update state
+  nodesState.val = nodes.val;
+  elementsState.val = elements.val;
+  nodeInputsState.val = nodeInputs;
+  elementInputsState.val = elementInputs;
+  deformOutputsState.val = deformOutputs;
 });
-const elementInputs: State<ElementInputs> = van.state({
-  elasticities: new Map([[0, 100]]),
-  thicknesses: new Map([[0, 1]]),
-  poissonsRatios: new Map([[0, 0.3]]),
-});
-
-const deformOutputs = van.state(
-  deform(nodes.val, elements.val, nodeInputs.val, elementInputs.val)
-);
 
 document.body.append(
   viewer({
     structure: {
-      nodes,
-      elements,
-      nodeInputs,
-      elementInputs,
-      deformOutputs,
+      nodes: nodesState,
+      elements: elementsState,
+      nodeInputs: nodeInputsState,
+      elementInputs: elementInputsState,
+      deformOutputs: deformOutputsState,
+    },
+    settingsObj: {
+      deformedShape: true,
     },
   })
 );
