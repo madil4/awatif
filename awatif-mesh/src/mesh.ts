7542e4359e0533251171716c75c79357982ef398
Kaison <cheungsiukaison.soniacheung@gmail.com>
2025-03-02 00:50:53 -0500

    Modify mesh function for color-map functions

--- awatif-mesh/src/mesh.ts
+++ awatif-mesh/src/mesh.ts
@@ -9,12 +9,27 @@ import { subdivide } from "./subdivide";
 const isWsLoaded = van.state(false);
 triangle.init(triangleWasmUrl).then(() => (isWsLoaded.val = true));
 
+/**
+ * Return an array of meshed nodes and elements.
+ *
+ * @remarks
+ * The meshed nodes are in [x, y, z] format on the X-Z plane with y = 0.
+ *
+ * @param points Array of point(s) in the form of [x, y].
+ * @param polygon Array of the indices in the points array in the form of [i_p1, i_p2, i_p3, ...].
+ */
 export function mesh({
   points,
   polygon,
+  maxMeshSize = 3,
+  maxNumSteinerPoints = 300,
+  minMeshAngleDegrees = 30,
 }: {
   points: State<number[][]>;
   polygon: State<number[]>;
+  maxMeshSize?: number;
+  maxNumSteinerPoints?: number;
+  minMeshAngleDegrees?: number;
 }): {
   nodes: State<Node[]>; // the output are reactive just to react after wasm is loaded
   elements: State<Element[]>;
@@ -34,7 +49,13 @@ export function mesh({
     });
     const output = triangle.makeIO();
 
-    triangle.triangulate("pzQOS300q30a3", input, output);
+    triangle.triangulate(
+      `pzQOS${maxNumSteinerPoints}q${minMeshAngleDegrees}${
+        maxMeshSize != null ? "a" : null
+      }${maxMeshSize ?? ""}`,
+      input,
+      output
+    );
 
     const { points: subPoints, faces: subFaces } = subdivide({
       points: triOutputToPoints(output),
