ac4f1ece7f889b33683ad6a1e4f50623d9bcb677
Mohamed Adil <me@mohamedadil.com>
2025-03-07 19:53:36 +0100

    polish plate example

--- examples/src/plate/main.ts
+++ examples/src/plate/main.ts
@@ -6,9 +6,15 @@ import {
   ElementInputs,
   DeformOutputs,
 } from "awatif-data-structure";
-import { viewer } from "awatif-ui";
+import { viewer, Parameters, parameters } from "awatif-ui";
 import { deform } from "awatif-fem";
 import { mesh } from "awatif-mesh";
+
+// Init
+const params: Parameters = {
+  xPosition: { value: van.state(15), min: 5, max: 20 },
+  load: { value: van.state(-50), min: -100, max: 100, step: 1 },
+};
 
 const nodesState: State<Node[]> = van.state([]);
 const elementsState: State<Element[]> = van.state([]);
@@ -16,30 +22,32 @@ const nodeInputsState: State<NodeInputs> = van.state({});
 const elementInputsState: State<ElementInputs> = van.state({});
 const deformOutputsState: State<DeformOutputs> = van.state({});
 
+// Events: on parameter change
 van.derive(() => {
   const { nodes, elements, boundaryIndices } = mesh({
     points: van.state([
-      [0, 0],
-      [10, 0],
-      [15, 5],
-      [0, 20],
-      [5, 2.5],
+      [0, 0, 0],
+      [15, 0, 0],
+      [params.xPosition.value.val, 10, 0],
+      [0, 5, 0],
     ]),
     polygon: van.state([0, 1, 2, 3]),
     maxMeshSize: 2,
   });
 
   const nodeInputs: NodeInputs = {
-    // use boundaryIndices to set boundary conditions
     supports: new Map(
       boundaryIndices.val.map((i) => [i, [true, true, true, true, true, true]])
     ),
-    loads: new Map(nodes.val.map((_, i) => [i, [0, 0, -50, 0, 0, 0]])),
+    loads: new Map(
+      nodes.val.map((_, i) => [i, [0, 0, params.load.value.val, 0, 0, 0]])
+    ),
   };
+  const elementsVal = elements.val;
   const elementInputs: ElementInputs = {
-    elasticities: new Map(elements.val.map((_, i) => [i, 100])),
-    thicknesses: new Map(elements.val.map((_, i) => [i, 1])),
-    poissonsRatios: new Map(elements.val.map((_, i) => [i, 0.3])),
+    elasticities: new Map(elementsVal.map((_, i) => [i, 100])),
+    thicknesses: new Map(elementsVal.map((_, i) => [i, 1])),
+    poissonsRatios: new Map(elementsVal.map((_, i) => [i, 0.3])),
   };
 
   const deformOutputs = deform(
@@ -58,6 +66,7 @@ van.derive(() => {
 });
 
 document.body.append(
+  parameters(params),
   viewer({
     structure: {
       nodes: nodesState,
