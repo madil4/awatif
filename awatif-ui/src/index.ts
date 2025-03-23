99010277a9be87d5b8238ab82f5a088e671c8452
Mohamed Adil <me@mohamedadil.com>
2025-03-23 12:41:15 +0100

    Resolve conficts with main

--- awatif-ui/src/index.ts
+++ awatif-ui/src/index.ts
@@ -5,6 +5,7 @@ export { getViewer } from "./viewer/getViewer";
 export { getParameters } from "./parameters/getParameters";
 export { getTables } from "./tables/getTables";
 export { getTable } from "./table/getTable";
+export { getLegend } from "./color-map/getLegend";
 export { getColorMap } from "./color-map/getColorMap";
 export { getToolbar } from "./toolbar/getToolbar";
 export { getDialog } from "./dialog/getDialog";
