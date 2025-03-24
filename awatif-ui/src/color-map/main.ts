04c02942a49b125264586280af93d785c8d129a1
Mohamed Adil <me@mohamedadil.com>
2025-03-24 23:39:15 +0100

    setup a colormap unit-test

--- awatif-ui/src/color-map/main.ts
+++ awatif-ui/src/color-map/main.ts
@@ -1,65 +1,28 @@
-import * as THREE from "three";
-import * as math from "mathjs";
 import van from "vanjs-core";
 
 import { Node } from "awatif-data-structure";
-import { getParameters, Parameters, getViewer, getColorMap } from "awatif-ui";
+import { getViewer, getColorMap } from "awatif-ui";
 import { getLegend } from "./getLegend";
 
-// Init
-const parameters: Parameters = {
-  boundary: {
-    value: van.state(10),
-    min: 1,
-    max: 10,
-    step: 0.1,
-    label: "Boundary point",
-  },
-};
+const objects3D = van.state([]);
+const values = van.state([]);
 
-const objects3D = van.state([new THREE.Mesh()]);
-const distancesState = van.state([]);
-
-// Events: on parameter change
 van.derive(() => {
   const points = van.state([
     [0, 0, 0],
     [5, 0, 0],
-    [parameters.boundary.value.val, 0, 3],
-    [8, 0, 7],
-    [15, 0, 5],
-    [15, 0, 0],
-    [20, 0, 0],
-    [20, 0, 10],
-    [0, 0, 10],
-    [0, 0, 0],
     [5, 0, 5],
+    [0, 0, 5],
   ] as Node[]);
-  const polygon = van.state([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  const polygon = van.state([0, 1, 2, 3]);
+  values.val = [0, 1, 2, 1];
 
-  distancesState.val = getDistancesFromVertex(
-    [parameters.boundary.value.val, 0, 3],
-    points.val
-  );
-  objects3D.val = [getColorMap(points, polygon, distancesState).val];
+  objects3D.val = [getColorMap(points, polygon, values).val];
 });
 
 document.body.append(
-  getLegend(distancesState, 10),
-  getParameters(parameters),
+  getLegend(values, 10),
   getViewer({
     objects3D,
   })
 );
-
-// Utils
-function getDistancesFromVertex(
-  vertex: [number, number, number],
-  points: Node[]
-): number[] {
-  return points.map((point) => {
-    return math.norm(
-      math.subtract(point, vertex) as math.MathCollection
-    ) as number;
-  });
-}
