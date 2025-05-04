53b068f2b434956f57113465015bde9004dfd415
Mohamed Adil <me@mohamedadil.com>
2025-05-04 17:37:06 +0200

    fix frame stiffness matrix bug

--- awatif-fem/src/cpp/utils/getTransformationMatrix.cpp
+++ awatif-fem/src/cpp/utils/getTransformationMatrix.cpp
@@ -6,6 +6,25 @@
 #include <iomanip>
 #include <stdexcept>
 
+// Declarations
+Eigen::MatrixXd getTransformationMatrixFrame(const Node &n0, const Node &n1);
+Eigen::MatrixXd getTransformationMatrixPlate(const Node &n1, const Node &n2, const Node &n3);
+
+Eigen::MatrixXd getTransformationMatrix(const std::vector<Node> &nodes)
+{
+    if (nodes.size() == 2)
+    {
+        return getTransformationMatrixFrame(nodes[0], nodes[1]);
+    }
+
+    if (nodes.size() == 3)
+    {
+        return getTransformationMatrixPlate(nodes[0], nodes[1], nodes[2]);
+    }
+
+    return Eigen::MatrixXd();
+}
+
 // Utils
 Eigen::MatrixXd getTransformationMatrixFrame(const Node &n0, const Node &n1)
 {
@@ -45,51 +64,36 @@ Eigen::MatrixXd getTransformationMatrixFrame(const Node &n0, const Node &n1)
     return t;
 }
 
-Eigen::MatrixXd getTransformationMatrix(const std::vector<Node> &nodes)
+Eigen::MatrixXd getTransformationMatrixPlate(const Node &n1, const Node &n2, const Node &n3)
 {
-    if (nodes.size() == 2)
+    auto getAverage = [](const std::vector<Node> &nodes)
     {
-        return getTransformationMatrixFrame(nodes[0], nodes[1]);
+        Eigen::Vector3d sum = Eigen::Vector3d::Zero();
+        for (const auto &node : nodes)
+        {
+            sum += Eigen::Vector3d(node[0], node[1], node[2]);
+        }
+        return sum / static_cast<double>(nodes.size());
+    };
+
+    Eigen::Vector3d j = getAverage({n2, n3});
+    Eigen::Vector3d k = getAverage({n1, n3});
+    Eigen::Vector3d i = getAverage({n1, n2});
+    Eigen::Vector3d x = (j - k).normalized();
+    Eigen::Vector3d r = (Eigen::Vector3d(n3[0], n3[1], n3[2]) - i).normalized();
+    Eigen::Vector3d z = x.cross(r).normalized();
+    Eigen::Vector3d y = z.cross(x).normalized();
+
+    Eigen::MatrixXd lambda(3, 3);
+    lambda << x(0), y(0), z(0),
+        x(1), y(1), z(1),
+        x(2), y(2), z(2);
+
+    Eigen::MatrixXd t = Eigen::MatrixXd::Zero(18, 18); // Initialize an 18x18 zero matrix
+    for (int i = 0; i < 6; ++i)
+    {
+        t.block<3, 3>(i * 3, i * 3) = lambda;
     }
 
-    // if (nodes.size() == 3)
-    // {
-    //     return getTransformationMatrixPlate(nodes[0], nodes[1], nodes[2]);
-    // }
-
-    return Eigen::MatrixXd();
+    return t;
 }
-
-// Eigen::MatrixXd getTransformationMatrixPlate(const Node &n1, const Node &n2, const Node &n3)
-// {
-//     auto getAverage = [](const std::vector<Node> &nodes)
-//     {
-//         Eigen::Vector3d sum = Eigen::Vector3d::Zero();
-//         for (const auto &node : nodes)
-//         {
-//             sum += Eigen::Vector3d(node[0], node[1], node[2]);
-//         }
-//         return sum / static_cast<double>(nodes.size());
-//     };
-
-//     Eigen::Vector3d j = getAverage({n2, n3});
-//     Eigen::Vector3d k = getAverage({n1, n3});
-//     Eigen::Vector3d i = getAverage({n1, n2});
-//     Eigen::Vector3d x = (j - k).normalized();
-//     Eigen::Vector3d r = (Eigen::Vector3d(n3[0], n3[1], n3[2]) - i).normalized();
-//     Eigen::Vector3d z = x.cross(r).normalized();
-//     Eigen::Vector3d y = z.cross(x).normalized();
-
-//     Eigen::MatrixXd lambda(3, 3);
-//     lambda << x(0), y(0), z(0),
-//         x(1), y(1), z(1),
-//         x(2), y(2), z(2);
-
-//     Eigen::MatrixXd t = Eigen::MatrixXd::Zero(18, 18); // Initialize an 18x18 zero matrix
-//     for (int i = 0; i < 6; ++i)
-//     {
-//         t.block<3, 3>(i * 3, i * 3) = lambda;
-//     }
-
-//     return t;
-// }
