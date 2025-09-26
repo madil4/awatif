0de6caaa5c980e4bdc0230d10c6c74a22200f2c8
Mohamed Adil <me@mohamedadil.com>
2025-09-26 22:54:50 +0200

    Remove website

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -6,29 +6,6 @@ export default defineConfig({
     port: 4600,
     open: "slab-designer/index.html",
   },
-  base: "./", // to resolve assets
   root: "./src",
-  build: {
-    outDir: "../../website/src/examples",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        "3d-structure": "src/3d-structure/index.html",
-        "advanced-truss": "src/advanced-truss/index.html",
-        beams: "src/beams/index.html",
-        curves: "src/curves/index.html",
-        "1d-mesh": "src/1d-mesh/index.html",
-        truss: "src/truss/index.html",
-        tables: "src/tables/index.html",
-        "2d-mesh": "src/2d-mesh/index.html",
-        drawing: "src/drawing/index.html",
-        report: "src/report/index.html",
-        plate: "src/plate/index.html",
-        building: "src/building/index.html",
-        "slab-designer": "src/slab-designer/index.html",
-        "color-map": "src/color-map/index.html",
-      },
-    },
-  },
   plugins: [topLevelAwait()], // used by awatif-fem & awatif-mesh to load wasm at top level
 });
