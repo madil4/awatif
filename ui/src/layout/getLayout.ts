c38edf0d409f7e97f310442abf0604e7937dc94e
Mohamed Adil <me@mohamedadil.com>
2025-11-08 13:26:18 +0100

    feat: implement layout component

---
+++ ui/src/layout/getLayout.ts
@@ -0,0 +1,29 @@
+import { html, render } from "lit-html";
+
+import "./styles.css";
+
+export function getLayout({
+  header,
+  display,
+  footer,
+}: {
+  header: HTMLElement;
+  display: HTMLElement;
+  footer: HTMLElement;
+}): HTMLElement {
+  const container = document.createElement("div");
+
+  const template = html`
+    <div id="layout">
+      <div class="top">
+        <div class="display">${display}</div>
+        <div class="header">${header}</div>
+      </div>
+      <div class="footer">${footer}</div>
+    </div>
+  `;
+
+  render(template, container);
+
+  return container.firstElementChild as HTMLElement;
+}
