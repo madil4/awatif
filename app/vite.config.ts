44c717d9b8346a6375c17e2376a9d97247fe6740
Mohamed Adil <me@mohamedadil.com>
2025-11-25 17:44:52 +0700

    feat: create new Awatif app with initial setup and basic geometry functionality

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,16 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 4600,
-    open: "geometry/index.html",
+    open: true,
   },
   root: "./src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        geometry: "./src/geometry/index.html",
-      },
-    },
   },
 });
