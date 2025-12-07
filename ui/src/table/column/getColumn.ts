fc2e3b26b2f3518aa874203aa4f8051796c338d0
Kaison <cheungsiukaison.soniacheung@gmail.com>
2025-12-07 01:44:42 -0500

    Set up basic grid structure

--- ui/src/table/column/getColumn.ts
+++ ui/src/table/column/getColumn.ts
@@ -1,10 +1,25 @@
 import { html, render } from "lit-html";
 import van, { State } from "vanjs-core";
+import { getCell } from "../cell/getCell";
 
 import "./styles.css";
 
-export function getRow(): HTMLElement {
-  const container = document.createElement("div");
+export function getColumn(): HTMLElement {
+  const container = document.createElement("td");
+
+  const template = () => {
+    return html`
+      ${getCell({
+        value: van.state("hello"),
+        id: van.state("A1"),
+        isEditMode: van.state(false),
+      })}
+    `;
+  };
+
+  van.derive(() => {
+    render(template(), container);
+  });
 
   return container as HTMLElement;
 }
