5ab9bb763f2d56663dda5076063a3ee26ca7d100
Mohamed Adil <me@mohamedadil.com>
2025-12-25 15:43:54 +0300

    feat: add components support in layout with styling and integration

--- ui/index.ts
+++ ui/index.ts
@@ -1,6 +1,7 @@
 export { getLayout } from "./layout/getLayout";
 export { getViewer } from "./viewer/getViewer";
 export { getDisplay } from "./display/getDisplay";
+export { getComponents } from "./components/getComponents";
 export { getTooltips } from "./tooltips/getTooltips";
 export { getTables } from "./tables/getTables";
 export { getToolbar } from "./toolbar/getToolbar";
