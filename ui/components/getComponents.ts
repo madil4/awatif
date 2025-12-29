ed0f382e7a0fdea9f43182ee6dc30c57239c2305
Mohamed Adil <me@mohamedadil.com>
2025-12-29 14:56:32 +0300

    fix: stop clearing geometry selection to empty array when active index is not null

--- ui/components/getComponents.ts
+++ ui/components/getComponents.ts
@@ -74,9 +74,7 @@ export function getComponents({
   });
 
   van.derive(() => {
-    if (activeIndex.val !== null)
-      geometry.selection.val = { points: [], lines: [] };
-    else geometry.selection.val = null;
+    if (activeIndex.val === null) geometry.selection.val = null;
   });
 
   const template = () => html`
