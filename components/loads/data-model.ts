2245a502bde7928f1559b065637f103326e3c8eb
Mohamed Adil <me@mohamedadil.com>
2026-01-10 22:10:06 +0100

    feat: enhance point load functionality by adding 3D object visualization and integrating component management in viewer

--- components/loads/data-model.ts
+++ components/loads/data-model.ts
@@ -1,5 +1,6 @@
 import { State } from "vanjs-core";
 import { TemplateResult } from "lit-html";
+import * as THREE from "three";
 
 // template
 export type LoadTemplate<Params extends Record<string, unknown>> = {
@@ -7,12 +8,21 @@ export type LoadTemplate<Params extends Record<string, unknown>> = {
   defaultParams: Params;
 
   getTemplate: ({ params }: { params: State<Params> }) => TemplateResult;
+
   getLoad: ({ params }: { params: Params }) => {
     // Load values: [Fx, Fy, Fz, Mx, My, Mz]
     // Fx, Fy, Fz: Force components in x, y, z directions
     // Mx, My, Mz: Moment components about x, y, z axes
     load: [number, number, number, number, number, number];
   };
+
+  getObject3D?: ({
+    params,
+    position,
+  }: {
+    params: Params;
+    position: [number, number, number];
+  }) => THREE.Object3D;
 };
 
 // components
