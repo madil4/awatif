dd7f78668d33a69cbe227199a822121eca852d20
Mohamed Adil <me@mohamedadil.com>
2026-01-10 17:14:22 +0100

    fix: update point load parameters to reflect correct force directions

--- components/loads/point-load/pointLoad.ts
+++ components/loads/point-load/pointLoad.ts
@@ -14,81 +14,54 @@ export const pointLoad: LoadTemplate<PointLoadParams> = {
   name: "Point Load",
   defaultParams: {
     Fx: 0,
-    Fy: 0,
-    Fz: -100, // Default downward force
+    Fy: -100,
+    Fz: 0,
     Mx: 0,
     My: 0,
     Mz: 0,
   },
 
   getTemplate: ({ params }) => {
-    return html`<div>
-      <label>Force X (Fx):</label>
-      <input
-        type="number"
-        .value=${params.val.Fx}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            Fx: Number((e.target as HTMLInputElement).value),
-          })}
-      />
+    return html`
+      <div>
+        <label>Force X:</label>
+        <input
+          type="number"
+          .value=${params.val.Fx}
+          @input=${(e: Event) =>
+            (params.val = {
+              ...params.val,
+              Fx: Number((e.target as HTMLInputElement).value),
+            })}
+        />
+      </div>
 
-      <label>Force Y (Fy):</label>
-      <input
-        type="number"
-        .value=${params.val.Fy}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            Fy: Number((e.target as HTMLInputElement).value),
-          })}
-      />
+      <div>
+        <label>Force Y:</label>
+        <input
+          type="number"
+          .value=${params.val.Fy}
+          @input=${(e: Event) =>
+            (params.val = {
+              ...params.val,
+              Fy: Number((e.target as HTMLInputElement).value),
+            })}
+        />
+      </div>
 
-      <label>Force Z (Fz):</label>
-      <input
-        type="number"
-        .value=${params.val.Fz}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            Fz: Number((e.target as HTMLInputElement).value),
-          })}
-      />
-
-      <label>Moment X (Mx):</label>
-      <input
-        type="number"
-        .value=${params.val.Mx}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            Mx: Number((e.target as HTMLInputElement).value),
-          })}
-      />
-
-      <label>Moment Y (My):</label>
-      <input
-        type="number"
-        .value=${params.val.My}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            My: Number((e.target as HTMLInputElement).value),
-          })}
-      />
-
-      <label>Moment Z (Mz):</label>
-      <input
-        type="number"
-        .value=${params.val.Mz}
-        @input=${(e: Event) =>
-          (params.val = {
-            ...params.val,
-            Mz: Number((e.target as HTMLInputElement).value),
-          })}
-      />
-    </div>`;
+      <div>
+        <label>Moment Z:</label>
+        <input
+          type="number"
+          .value=${params.val.Mz}
+          @input=${(e: Event) =>
+            (params.val = {
+              ...params.val,
+              Mz: Number((e.target as HTMLInputElement).value),
+            })}
+        />
+      </div>
+    `;
   },
 
   getLoad: ({ params }) => {
