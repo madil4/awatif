27254c1e2d2b1b0b2dad9d0bf20cba87e310ac93
Mohamed Adil <me@mohamedadil.com>
2026-01-14 15:19:33 +0100

    Rename toolbarMode to componentsBarMode in main.ts and related files for improved clarity and consistency in component handling.

--- ui/index.ts
+++ ui/index.ts
@@ -4,7 +4,6 @@ export { getDisplay } from "./display/getDisplay";
 export { getComponents } from "./components/getComponents";
 export { getTooltips } from "./tooltips/getTooltips";
 export { getTables } from "./tables/getTables";
-export { ToolbarMode } from "./components/componentsBar/getComponentsBar";
 export { getReport } from "./report/getReport";
 
 export type { Display } from "./display/getDisplay";
