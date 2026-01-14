24577d60fe1ae452572f3560a1b3945d01542c9f
Mohamed Adil <me@mohamedadil.com>
2026-01-14 21:26:30 +0100

    Replace RC Column template with EN RC Column design template and add corresponding implementation. Introduced new design parameters and rendering logic for EN RC Column in enRcColumn.ts, updating the templates map accordingly.

--- components/templates.ts
+++ components/templates.ts
@@ -2,12 +2,12 @@ import { ComponentsType } from "./data-model";
 import { lineMesh } from "./mesh/line-mesh/lineMesh";
 import { pointLoad } from "./loads/point-load/pointLoad";
 import { pointSupport } from "./supports/point-support/pointSupport";
-import { rcColumn } from "./design/rc-column/rcColumn";
+import { enRcColumn } from "./design/en-rc-column/enRcColumn";
 
 // Todo: use a map with ids to make it possible to remove templates without reindexing
 export const templates = new Map<ComponentsType, any[]>([
   [ComponentsType.MESH, [lineMesh]],
   [ComponentsType.LOADS, [pointLoad]],
   [ComponentsType.SUPPORTS, [pointSupport]],
-  [ComponentsType.DESIGN, [rcColumn]],
+  [ComponentsType.DESIGN, [enRcColumn]],
 ]);
