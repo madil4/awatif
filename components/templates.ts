34e9d6c18056f8a8ea79a57100304ada4df216a0
Mohamed Adil <me@mohamedadil.com>
2026-01-20 09:52:46 +0100

    refactor: centralize and export templates from `@awatif/components`

---
+++ components/templates.ts
@@ -0,0 +1,12 @@
+import { lineMesh } from "./mesh/line-mesh/lineMesh";
+import { pointLoad } from "./loads/point-load/pointLoad";
+import { pointSupport } from "./supports/point-support/pointSupport";
+import { basic } from "./design/basic/basic";
+import { ComponentsType } from "./data-model";
+
+export const templates = new Map<ComponentsType, Map<string, any>>([
+  [ComponentsType.MESH, new Map([["line-mesh", lineMesh]])],
+  [ComponentsType.LOADS, new Map([["point-load", pointLoad]])],
+  [ComponentsType.SUPPORTS, new Map([["point-support", pointSupport]])],
+  [ComponentsType.DESIGN, new Map([["basic", basic]])],
+]);
