837d076284529e679432aa3719d8ed96f423c812
Mohamed Adil <me@mohamedadil.com>
2026-01-28 14:33:57 +0100

    feat: Conditionally display the RC column length factor input based on the active analysis type.

--- ui/components/getComponents.ts
+++ ui/components/getComponents.ts
@@ -45,6 +45,7 @@ export function getComponents({
     components,
     componentsBarMode,
     templates,
+    activeAnalysis,
   });
 
   const componentsBar = getComponentsBar({ componentsBarMode, activeAnalysis });
